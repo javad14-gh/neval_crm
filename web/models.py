7941c5bd9f9dd0e7d3af8a66aaab41266f1b2dea
javad14-gh <javad14396@gmail.com>
2023-01-09 00:21:20 +0300

    - add customers model - add migrations to .gitignore

--- web/models.py
+++ web/models.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 class category(models.Model):
@@ -20,8 +19,14 @@ class buy_from(models.Model):
     def __str__(self):
         return f'{self.name}-{self.type}'
 
+class customers(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class orders(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(customers,on_delete=models.CASCADE)
     date = models.DateField()
     category = models.ManyToManyField(category)
     amount = models.BigIntegerField()
@@ -57,5 +62,3 @@ class otherCosts(models.Model):
     order = models.ManyToManyField(orders)
     def __str__(self):
         return self.text
-
-
