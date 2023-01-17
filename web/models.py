3108e93c07bd55b80b1464cc599ba706c1167d5e
javad14-gh <javad14396@gmail.com>
2023-01-17 16:32:57 +0300

    -create remained func -improve filter page -create follow code for order model

--- web/models.py
+++ web/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from hashlib import sha1
+from time import time
 
 # Create your models here.
 class category(models.Model):
@@ -32,9 +34,10 @@ class orders(models.Model):
     category = models.ManyToManyField(category)
     amount = models.BigIntegerField()
     text = models.CharField(max_length=255,blank=True)
-
+    s = int(sha1(str(time()).encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+    followCode = models.CharField(default=s,auto_created=True,unique=True,max_length=255)
     def __str__(self):
-        return f'{self.date}-{self.user}'
+        return self.followCode
 
 class changes(models.Model):
     order = models.ManyToManyField(orders)
@@ -42,6 +45,7 @@ class changes(models.Model):
     rate = models.IntegerField()
     rial = models.BigIntegerField()
     lir = models.IntegerField()
+    
 
     def __str__(self):
         return f'{self.rate}'
@@ -49,13 +53,16 @@ class changes(models.Model):
 class buy(models.Model):
     order = models.ManyToManyField(orders)
     date = models.DateField()
-    amount = models.BigIntegerField()
+    amount = models.IntegerField()
     change = models.ManyToManyField(changes)
     category = models.ManyToManyField(category)
     buy_from = models.ForeignKey(buy_from,on_delete=models.CASCADE)
     text = models.CharField(max_length=255,blank=True)
     def __str__(self):
         return f'{self.date}-{self.buy_from}-{self.amount}'
+    @property
+    def sud(self):
+        return self.amount * 0.3
 
 class otherCosts(models.Model):
     text = models.CharField(max_length=255)
--- web/views.py
+++ web/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
-from .models import changes
+from .models import buy , orders , changes
 from django.db.models import Sum
 # Create your views here.
 
+def remained(oredrID):
+    this_order = orders.objects.filter(followCode = oredrID).get()
+    buys = buy.objects.filter(order = this_order)
+    lirs = changes.objects.filter(order = this_order).aggregate(Sum('lir'))
+    mande = lirs['lir__sum']
+    for x in buys:
+        mande -= (x.sud)+ (x.amount)
+
+    return mande
 
 def submit_buy(request):
+    context = {}
     a = request.GET
     if a == {}:
-        context = 'choose rate!!!!!'
+        context = {}
     else:
-        context = changes.objects.filter(rate = a['rate'])
-    return render(request,template_name= 'filter.html',context={'sum':context})
+        this_id = a['rate']
+        this_order = orders.objects.filter(followCode = this_id).get()
+        context['sum'] = changes.objects.filter(order = this_order)
+        context['mande'] = remained(this_id)
+    return render(request,template_name= 'filter.html',context=context)
 
 
 def home(request):
